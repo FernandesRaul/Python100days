3b9992bbfe38a962ba0a9802972f2b761070606a
Raul <raulfernandes57@gmail.com>
2024-06-18 18:23:22 -0300

    new file

---
+++ motorcycle_club_name_generator.py
@@ -0,0 +1,8 @@
+# Makes questions to the users to create their motorcycle club name.
+print("Welcome to the Motorcycle Club names generator")
+
+city_name = input("In which city and state do you live in?\n")
+favourite_animal = input("What's your favourite animal?\n")
+generic_name = "Road"
+
+print("Your Motorcycle Club name could be : " + favourite_animal + "s of the " + generic_name + " / " + city_name)
